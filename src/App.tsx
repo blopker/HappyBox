4f7e9e5ab15cdf6a3dfe36a4aef5983430a0ae47
Bo Lopker <lopkerk@gmail.com>
2024-12-17 23:29:11 +0000

    touch

--- src/App.tsx
+++ src/App.tsx
@@ -25,6 +25,9 @@ function HappyBoxes() {
       onMouseMove={() => {
         setCorlors(getColors());
       }}
+      onTouchMove={() => {
+        setCorlors(getColors());
+      }}
     >
       <For each={colors()}>{(i) => <HappyBox color={i} />}</For>
     </div>
